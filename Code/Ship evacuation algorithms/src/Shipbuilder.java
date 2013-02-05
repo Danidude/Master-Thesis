5815de3dae424d09cde494c41f90790fcc476608
Daniel Aasen <aasen144@hotmail.com>
2013-02-05 17:45:45 +0100

    Now creats the rooms for the fifth flor

--- Code/Ship evacuation algorithms/src/Shipbuilder.java
+++ Code/Ship evacuation algorithms/src/Shipbuilder.java
@@ -1,32 +1,45 @@
+import java.util.ArrayList;
+import java.util.Map;
+
 
 public class Shipbuilder {
-	
+	int nodeID = 0;
 	public Ship createCelebrityXpedition()
 	{
 		Ship celebX = new Ship(4);
-		return null;
+		celebX = createPanormaFlor(celebX);
+		return celebX;
 	}
 	
-	private Ship createPanormaFlor()
+	private Ship createPanormaFlor(Ship s)
 	{
 		/**
 		 * Number of rooms: 9x2
 		 * Number of hallways:10
-		 * Number of exits:1
 		 * Number of diningroom:10
 		 * Number of steairs:3
 		 * 
-		 * Number of total nodes: 42
+		 * Number of total nodes: 41
 		 */
 		
+		ArrayList<Node> tempGUESTROOMList = createRoom(Node.NodeType.GUESTROOM, 18);
+		ArrayList<Node> tempDININGROOMList = createRoom(Node.NodeType.DININGROOM, 10);
+		ArrayList<Node> tempHALLWAYList = createRoom(Node.NodeType.HALLWAY, 10);
+		ArrayList<Node> tempSTAIRSList = createRoom(Node.NodeType.STAIRS, 3);
+		
+		addList(tempGUESTROOMList, s.fifthFlor);
+		addList(tempDININGROOMList, s.fifthFlor);
+		addList(tempHALLWAYList, s.fifthFlor);
+		addList(tempSTAIRSList, s.fifthFlor);
 		
 		
-		return null;
+		return s;
 	}
 	
-	private Node createRoom(Node.NodeType nt, int NodeID)
+	private ArrayList<Node> createRoom(Node.NodeType nt, int numberOfRooms)
 	{
 		int capacity = 0;
+		ArrayList<Node> tempList = new ArrayList<Node>();
         
         switch (nt) {
         case STAIRS: capacity = 10; break;
@@ -36,8 +49,21 @@ public class Shipbuilder {
         case GAMEROOM: capacity = 10; break;
         case SHOP: capacity = 10; break;
         }
-        Node n = new Node(NodeID, nt, capacity);
+        for(int room = 0; room<numberOfRooms; room++)
+        {
+        	Node n = new Node(nodeID++, nt, capacity);
+        	tempList.add(n);
+        }
         
-		return n;
+        
+		return tempList;
+	}
+	
+	private void addList(ArrayList<Node> list, Map<Integer, Node> map)
+	{
+		for(Node n:list)
+		{
+			map.put(n.NodeID, n);
+		}
 	}
 }
