d98d146f146c75906a601ec68eba1a5437230103
Steinsholt <steinsholt85@hotmail.com>
2013-02-24 23:03:36 +0100

    Dijkstra's now works by calculating paths from a list of sources to a list of exists, one bug persists though

--- Code/Ship evacuation algorithms/src/Dijkstra.java
+++ Code/Ship evacuation algorithms/src/Dijkstra.java
@@ -5,15 +5,18 @@ import java.util.PriorityQueue;
 
 
 public class Dijkstra {
+	Node currentNode = null;
 	public void findPath(Node source){
+
+		currentNode = source;
 		
 		// The shortest distance from the source node to this node.
 		source.minDistance = 0;
-		
+
 		// Visit each node starting with the smallest minDistance
 		PriorityQueue<Node> nodeQueue = new PriorityQueue<Node>();
 		nodeQueue.add(source);
-		
+
 		while (!nodeQueue.isEmpty()){
 			Node current = nodeQueue.poll();
 			
@@ -21,7 +24,7 @@ public class Dijkstra {
 			for (Edge e : current.getPaths()){
 				Node destination = e.getNode();
 				int weight = e.getWeight();
-				
+
 				// 
 				double distance = current.minDistance + weight;
 				if(distance < destination.minDistance){
@@ -33,12 +36,35 @@ public class Dijkstra {
 			}
 		}
 	}
+	
+	// Finds the shortest path from the source node to the exit node
 	public List<Node> getShortestPath(Node exit){
 		List<Node> path = new ArrayList<Node>();
-	    for (Node node = exit; node != null; node = node.previous)
-	        path.add(node);
+		for (Node node = exit; node != null; node = node.previous)
+			path.add(node);
 
-	    Collections.reverse(path);
-	    return path;
+		Collections.reverse(path);
+		return path;
+	}
+	
+	// Finds the shortest path from the source node to a list of exit nodes
+	public void getShortestPaths(List<Node> exits){
+		for(Node n : exits){
+			System.out.print("The distance to node " + n.getID() + " from source node " + currentNode.getID() + " is " + n.minDistance);
+			List<Node> path = getShortestPath(n);
+			System.out.print(" and goes via Path: ");
+			for(Node v : path){
+				System.out.print(v.getID() + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	// Finds the shortest path from a list of source nodes to a list of exit nodes
+	public void getAllPaths(List<Node> sources, List<Node> exits){
+		for(Node s : sources){
+			findPath(s);
+			getShortestPaths(exits);
+		}
 	}
 }
--- Code/Ship evacuation algorithms/src/Run.java
+++ Code/Ship evacuation algorithms/src/Run.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -8,29 +9,32 @@ public class Run {
 		//Ship s = sb.createCelebrityXpedition();
 		//System.out.println(s.fifthFlor.size());
 		
-		Node n1 = new Node(0, null, 1, 1);
-		Node n2 = new Node(1, null, 1, 1);
-		Node n3 = new Node(2, null, 1, 1);
-		Node n4 = new Node(3, null, 1, 1);
+		// int NodeID, NodeType nt, int cap, int fNumber
+		Node n1 = new Node(1, null, 1, 1);
+		Node n2 = new Node(2, null, 1, 1);
+		Node n3 = new Node(3, null, 1, 1);
+		Node n4 = new Node(4, null, 1, 1);
 		
+		
+		// int flow, int edgeID, Node node, Ship s, int weight
 		n1.addEdge(new Edge(1, 1, n2, null, 1));
 		n1.addEdge(new Edge(1, 1, n3, null, 2));
+		n2.addEdge(new Edge(1, 1, n1, null, 1));
 		n2.addEdge(new Edge(1, 1, n4, null, 1));
+		n3.addEdge(new Edge(1, 1, n1, null, 2));
 		n3.addEdge(new Edge(1, 1, n4, null, 1));
+		n4.addEdge(new Edge(1, 1, n2, null, 1));
+		n4.addEdge(new Edge(1, 1, n3, null, 1));
 		
-		Node[] nodes = { n1, n2, n3, n4 };
+		List<Node> nodes = new ArrayList<Node>();
+		nodes.add(n1);
+		nodes.add(n2);
+		nodes.add(n3);
+		nodes.add(n4);
 		
 		Dijkstra d = new Dijkstra();
-		d.findPath(n1);
-		
-		for (Node n : nodes){
-			System.out.print("Distance to " + n.getID() + ": " + n.minDistance + " ");
-			List<Node> path = d.getShortestPath(n);
-			System.out.print("Path: ");
-			for(Node v : path){
-				System.out.print(v.getID() + " ");
-			}
-			System.out.println();
-		}
+		d.getAllPaths(nodes, nodes);
+		//d.findPath(n1);
+		//d.getShortestPaths(nodes);
 	}
 }
